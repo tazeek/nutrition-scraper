90c4d6fa1093582b3daf1d966a11cfdbcf3ab910
tazeek <tazeek.rakib@gmail.com>
2023-11-14 15:55:57 +1100

    Get the serving information

--- scrapers/woolworth_scraper.js
+++ scrapers/woolworth_scraper.js
@@ -19,3 +19,16 @@ size = servings_size.replace(/[^\d.]/g,'')
 // First value: Quantity per serving
 // Second value: Quantity per 100g/100mL
 nutrition_rows = nutrition_div_elements[2].getElementsByClassName('nutrition-row');
+
+// We ignore the header because we know what it is.
+for (let i = 1; i < nutrition_rows.length; i++) {
+
+    // Go row by row
+    nutrition_row = nutrition_rows[i].getElementsByClassName('nutrition-column');
+
+    // Extract: Metric/Nutrient, Serving (regular), Serinvg (per 100g/100mL)
+    metric = nutrition_row[0].innerText;
+    serving_quantity = nutrition_row[1].innerText;
+    serving_per_100 = nutrition_row[2].innerText;
+    
+  }
