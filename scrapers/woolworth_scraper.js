65a4c8310e318ae6fccd0a7b1b84269a078a160f
tazeek <tazeek.rakib@gmail.com>
2023-11-14 15:29:10 +1100

    Remove the gram symbol

--- scrapers/woolworth_scraper.js
+++ scrapers/woolworth_scraper.js
@@ -7,8 +7,11 @@ let nutrition_info_area = document.getElementsByClassName('ar-product-details-nu
 let nutrition_div_elements = nutrition_info_area.getElementsByTagName('div');
 
 // First div element: servings per package
-servings_per_pack = nutrition_div_elements[0].innerText.split(":")[1].trim()
+let servings_pack = nutrition_div_elements[0].innerText.split(":")[1].trim()
 
 // Second div element: serving size
+// Remove all non-alphabets
+let servings_size = nutrition_div_elements[1].innerText.split(":")[1].trim()
+servings_size = servings_size.replace(/[^\d.]/g,'')
 
 // Third div element: nutrition table
