e69bbb592dd97002d10958f7a1181b41d6d7924d
tazeek <tazeek.rakib@gmail.com>
2023-11-18 00:34:19 +1100

    Beauitfulsoup gets 403. Lets try headless approach

--- soup_test.py
+++ soup_test.py
@@ -1,2 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+import shutil
+
+def get_chromedriver_path():
+    return shutil.which('chromedriver')
