4c1c26932abfe716a6c53a33b642659023a7ede3
tazeek <tazeek.rakib@gmail.com>
2023-11-17 16:05:27 +1100

    Execute JS script function added

--- web_scraper.py
+++ web_scraper.py
@@ -1,6 +1,13 @@
-
+import json
 
 # A function to execute script (JS)
+def execute_script(browser, js_file):
+    f = open(js_file, 'r')
+    json_string = browser.execute_script(f.read())
+    f.close()
+
+    return json.loads(json_string)
+    
 
 # A function to sleep (scraping ethics)
 
