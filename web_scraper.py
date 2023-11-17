a4a1e9004c668fd735a51c89dd0eb19bb5cdd728
tazeek <tazeek.rakib@gmail.com>
2023-11-18 10:17:48 +1100

    Lets try with BS

--- web_scraper.py
+++ web_scraper.py
@@ -1,39 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup as soup
 
-import json
+import requests
 import time
 import random
-import shutil
+import re
 import pandas as pd
 
 class NutriScraper:
 
     def __init__(self):
 
-        options = Options()
-        options.add_argument("--headless")
+        self._header = {
+            'User-Agent': 'Mozilla/5.0'
+        }
 
-        self._browser = webdriver.Chrome(
-            service=Service(
-                executable_path=shutil.which('chromedriver')
-            ),
-            options=options
-        )
+    @classmethod
+    def get_value_html(cls, serve_str):
+        return serve_str.split(':')[1].strip()
 
-    def _execute_script(self, js_file):
+    @classmethod
+    def get_text(cls, nutri_val):
+        return re.sub('[^a-zA-Z ]', '', nutri_val).strip()
 
-        json_string = ''
-
-        with open(js_file, 'r') as f:
-            json_string = self._browser.execute_script(f.read())
-
-        return json.loads(json_string)
+    @classmethod
+    def get_number(cls, nutri_val):
+        return re.findall("\d+\.\d+", nutri_val)[0]
     
     @classmethod
     def _polite_delay(cls):
@@ -44,24 +35,54 @@ class NutriScraper:
     # Pass in the URL
     def perform_scraping(self, url):
 
-        self._browser.get(url)
+        nutrition_dict = {}
 
-        # Selenium only waits for the HTML DOM to load.
-        # We are scraping dynamically loaded content so we have to 
-        # explicitly make Selenium wait until this is loaded
-        # Wait until the presence of a HTML element with the class 
-        # 'ar-product-details-nutrition-table' is detected (Nutrition Table)
-        try:
-            timeout = 20
-            WebDriverWait(self._browser, timeout).until(EC.presence_of_element_located(
-                (By.CLASS_NAME, 'ar-product-details-nutrition-table')
-            ))
-        except TimeoutException:
-            pass
+        # Extract the html first
+        response = requests.get(url, headers=self._header)
+        page_soup = soup(response.text, 'html.parser')
 
-        nutrition_info = self._execute_script('scrapers/woolworth_scraper.js')
+        # Get the product name
+        product_name = page_soup.find_all("h1", {"class": "shelfProductTile-title heading4"})
+        nutrition_dict['Product Name'] = product_name[0].get_text()
+
+        # Get the serving pack
+        servings_pack = page_soup.find("div", {"*ngif": 'productServingsPerPack'})
+        servings_pack_value = self.get_value_html(servings_pack.get_text())
+        nutrition_dict['Servings per pack'] = servings_pack_value
+
+        # Get the serving size, followed by the metrics
+        serving_size = page_soup.find("div", {"*ngif": 'productServingSize'})
+        serving_size = self.get_value_html(serving_size.get_text())
+
+        serving_size_metric = self.get_text(serving_size) 
+        serving_size_value = self.get_number(serving_size)
+        nutrition_dict[f'Serving size {serving_size_metric}'] = serving_size_value
+
+        nutrition_dict['Pack Size'] = float(serving_size_value) * float(servings_pack_value)
+
+        nutrition_row = page_soup.find_all("ul", {"class": 'wow-row nutrition-row'})
+
+        for row in nutrition_row:
+
+            granular_vals = row.find_all("li", {"class": 'wow-col-4 nutrition-column'})
+            granular_vals = [val.get_text().strip() for val in granular_vals]
+
+            # Get the nutrition label
+            nutrition = self.get_text(granular_vals[0].strip())
+
+            # Get the metric and the respective numbers
+            serving_quantity = granular_vals[1]
+            serving_per_100 = granular_vals[2]
+
+            metric = self.get_text(serving_quantity)
+
+            serving_quantity = self.get_number(serving_quantity)
+            serving_per_100 = self.get_number(serving_per_100)
+
+            nutrition_dict[f'{nutrition} per serving ({metric})'] = serving_quantity
+            nutrition_dict[f'{nutrition} per 100g/100mL ({metric})'] = serving_per_100
 
         # Respect the scraping etiquettes :)
         self._polite_delay()
 
-        return nutrition_info
+        return nutrition_dict
